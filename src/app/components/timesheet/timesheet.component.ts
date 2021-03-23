b5bcc15d2a0e5e5896a274fa7ab97176161c0afa
RasKids <RasKids@macs-MacBook-Pro-2.local>
2021-03-22 21:14:10 -0600

    unit  8

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
+import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Employee } from 'src/app/interfaces/employee';
 
 @Component({
   selector: 'app-timesheet',
@@ -11,6 +13,9 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 export class TimesheetComponent implements OnInit {
   departments: Department[] = [];
   department: any;
+  employeeNameFC = new FormControl('', this.nameValidator());
+  employees: Employee[] = [];
+  employeeId = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,5 +27,34 @@ export class TimesheetComponent implements OnInit {
     this.department = this.departments.find(
       (department) => department.id === this.route.snapshot.params['id']
     );
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+    }
+  }
+
+  nameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      let error = null;
+      if (this.employees && this.employees.length) {
+        this.employees.forEach((employee) => {
+          if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+            error = { duplicate: true };
+          }
+        });
+      }
+      return error;
+    };
   }
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 // import { MatModule } from '@angular/material/'
-
 
 @NgModule({
   declarations: [],
@@ -16,14 +18,20 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatListModule
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatListModule
-  ]
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
