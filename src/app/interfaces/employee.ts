f80882c1c4368457735080b8ed02c06e96e2716c
RasKids <RasKids@macs-MacBook-Pro-2.local>
2021-03-22 21:41:04 -0600

    unit 8

--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -1,6 +1,14 @@
 export interface Employee {
+    [day: string]: any;
   id: string;
   departmentId: string;
   name: string;
   payRate: number;
+  monday: number;
+  tuesday: number;
+  wednesday: number;
+  thursday: number;
+  friday: number;
+  saturday: number;
+  sunday: number;
 }
